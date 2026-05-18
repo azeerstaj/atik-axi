8601b6cea22d04c6e5b688f7e4816ce3d5eb36ca
AhmedZeer <ahmed.zeer@std.yildiz.edu.tr>
2026-05-18 16:44:08 +0000

    workload diff + results

--- software/scripts/export_gpt2_prefill_cases.py
+++ software/scripts/export_gpt2_prefill_cases.py
@@ -37,17 +37,11 @@ class Gpt2PrefillCase:
 # Start with smaller ramp-up cases, then run the 512-oriented throughput cases.
 DEFAULT_CASES = (
     Gpt2PrefillCase("gpt2prefill_l1_s16_d64_h4_ff128_vocab128", 16, 64, 4, 128, 1, 128, 801),
-    Gpt2PrefillCase("gpt2prefill_l1_s32_d128_h4_ff256_vocab256", 32, 128, 4, 256, 1, 256, 802),
-    Gpt2PrefillCase("gpt2prefill_l1_s64_d128_h4_ff256_vocab256", 64, 128, 4, 256, 1, 256, 803),
-    # Gpt2PrefillCase("gpt2prefill_l4_s128_d128_h4_ff256_vocab256", 128, 128, 4, 256, 1, 256, 804),
-    # Gpt2PrefillCase("gpt2prefill_l2_s128_d256_h4_ff256_vocab256", 128, 256, 4, 256, 1, 256, 805, 0.25, 0.015625),
-    # Gpt2PrefillCase("gpt2prefill_l1_s256_d128_h4_ff256_vocab256", 256, 128, 4, 256, 1, 256, 806, 0.25, 0.015625),
-    Gpt2PrefillCase("gpt2prefill_512_l1_s128_d512_h8_ff512_vocab512", 128, 512, 8, 512, 1, 512, 811, 0.25, 0.015625),
-    Gpt2PrefillCase("gpt2prefill_512_l2_s128_d512_h8_ff512_vocab512", 128, 512, 8, 512, 1, 512, 812, 0.25, 0.015625),
-    Gpt2PrefillCase("gpt2prefill_512_l1_s256_d512_h8_ff512_vocab512", 256, 512, 8, 512, 1, 512, 813, 0.25, 0.015625),
-    Gpt2PrefillCase("gpt2prefill_512_l2_s256_d512_h8_ff512_vocab512", 256, 512, 8, 512, 1, 512, 814, 0.25, 0.015625),
-    Gpt2PrefillCase("gpt2prefill_512_l1_s512_d256_h4_ff512_vocab512", 512, 256, 4, 512, 1, 512, 815, 0.25, 0.015625),
-    Gpt2PrefillCase("gpt2prefill_512_l1_s512_d512_h8_ff512_vocab512", 512, 512, 8, 512, 1, 512, 816, 0.20, 0.01171875),
+    Gpt2PrefillCase("gpt2prefill_l1_s16_d64_h4_ff128_vocab128", 32, 64, 4, 128, 1, 128, 801),
+    Gpt2PrefillCase("gpt2prefill_l1_s32_d128_h4_ff256_vocab256", 64, 128, 4, 256, 1, 256, 802),
+    Gpt2PrefillCase("gpt2prefill_l1_s64_d128_h4_ff256_vocab256", 128, 128, 4, 256, 1, 256, 803),
+    Gpt2PrefillCase("gpt2prefill_512_l1_s128_d512_h8_ff512_vocab512", 256, 512, 8, 512, 1, 512, 811, 0.25, 0.015625),
+    Gpt2PrefillCase("gpt2prefill_512_l1_s512_d256_h4_ff512_vocab512", 512, 512, 4, 512, 1, 512, 815, 0.25, 0.015625),
 )
 
 
